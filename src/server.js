3044d4ff893430779b2de77d9ca0796950337799
Anh Hoang <>
2024-02-25 17:06:08 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+import initWebRoutes from "./routes/web";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//config view engine
+configViewEngine(app);
+
+//init web route
+initWebRoutes(app);
+
+app.listen(PORT, () => {
+    console.log(">>> JWT backend is running on port " + PORT);
+});
